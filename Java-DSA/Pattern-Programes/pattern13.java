c267ac7abb4d679e1b7fcaed75a064cd584d8748
Ayush Raturi <ayushraturi6679@gmail.com>
2026-03-21 12:58:18 +0530

    Pattern 13 added

---
+++ Java-DSA/Pattern-Programes/pattern13.java
@@ -0,0 +1,57 @@
+/* 
+
+* * * * * * * * * 
+* *     *     * *
+*   *   *   *   *
+*     * * *     *
+* * * * * * * * *
+*     * * *     *
+*   *   *   *   *
+* *     *     * *
+* * * * * * * * *
+
+
+*/
+
+
+
+import java.util.*;
+
+class pattern13 {
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Rows:");
+        int n = sc.nextInt();
+        sc.close();
+        pattern(n);
+    }
+
+
+ public static void pattern(int n){
+
+        // checking the odd or not
+      if (n%2 == 0) {
+    System.out.println("Please Enter odd Number to Execute the Pattern");
+        return;
+    } 
+
+        
+    int mid = n/2+1;
+         for (int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+
+                if(i==1 || j==1 || i==n || j==n || i==mid || j==mid || i+j == n+1 || i==j ){
+                     System.out.print("* "); 
+                    }
+                else{
+                    System.out.print("  ");
+                }
+                
+            }
+            System.out.println();  
+            
+        }
+    }
+}
