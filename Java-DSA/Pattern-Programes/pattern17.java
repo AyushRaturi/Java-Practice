69f0230c522cf03d22068ac8de9eb4ba180fc10a
Ayush Raturi <ayushraturi6679@gmail.com>
2026-03-22 14:44:05 +0530

    new pattern added

---
+++ Java-DSA/Pattern-Programes/pattern17.java
@@ -0,0 +1,50 @@
+/* Q22
+
+1
+2       3
+4       5       6
+7       8       9       10
+11      12      13      14      15
+
+*/
+
+
+import java.util.*;
+
+class pattern17{
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Rows:");
+        int n = sc.nextInt();
+        sc.close();
+        pattern(n);
+    }
+
+
+ public static void pattern(int n){
+
+    // checking the odd or not
+    /*  if (n%2 == 0) {
+    System.out.println("Please Enter odd Number to Execute the Pattern");
+        return;
+    } */
+
+
+    // int mid = n/2 + 1;
+    int patternsize = 1 ;
+    int m = 1;
+         for (int i=1;i<=n;i++){
+
+            for (int j=1;j<=patternsize;j++){
+                 System.out.print(m+++"\t"); 
+            }
+            patternsize++;
+            
+            System.out.println();  
+            
+        }
+    }
+}
+
