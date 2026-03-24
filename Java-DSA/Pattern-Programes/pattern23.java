194cecc3f12ddc60bb5ec6430a4299a594824d3b
Ayush Raturi <ayushraturi6679@gmail.com>
2026-03-24 13:14:22 +0530

    Patter 22 & 23 Added

---
+++ Java-DSA/Pattern-Programes/pattern23.java
@@ -0,0 +1,50 @@
+/* Q27
+
+O
+M       N
+J       K       L
+F       G       H       I
+A       B       C       D       E
+
+*/
+
+
+import java.util.*;
+
+class pattern23{
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Rows:");
+        int n = sc.nextInt();
+        sc.close();
+        pattern(n);
+    }
+
+
+ public static void pattern(int n){
+
+    // checking the odd or not
+    /*  if (n%2 == 0) {
+    System.out.println("Please Enter odd Number to Execute the Pattern");
+        return;
+    } */
+
+
+    // int mid = n/2 + 1;
+    int patternsize = 1 ;
+    int m = n*(n+1)/2;
+    for (int i=1;i<=n;i++){
+        
+            for (int j=1;j<=patternsize;j++){
+                 System.out.print( (char) (64+m++) +"\t"); 
+            }
+            m = m - (2*i+1);
+            patternsize++;
+            System.out.println();  
+            
+        }
+    }
+}
+
