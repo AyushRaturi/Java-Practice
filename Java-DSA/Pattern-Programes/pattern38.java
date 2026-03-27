2a13f6d28278733881c368076cbf387cd5bc18c0
Ayush Raturi <ayushraturi6679@gmail.com>
2026-03-27 23:41:02 +0530

    Pattern 37 & 38 Added

---
+++ Java-DSA/Pattern-Programes/pattern38.java
@@ -0,0 +1,64 @@
+/* Q76
+
+        5 
+      5 4 5
+    5 4 3 4 5
+  5 4 3 2 3 4 5
+5 4 3 2 1 2 3 4 5
+
+*/
+
+
+import java.util.Scanner;
+
+public class pattern38{
+
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Rows:");
+        int n = sc.nextInt();
+        sc.close();
+        pattern(n);
+    }
+
+    public static void pattern (int n){
+        // checking the odd or not
+    if (n%2 == 0) {
+        System.out.println("Please Enter odd Number to Execute the Pattern");
+        return;
+    }
+
+        int space = n-1;
+        int patternsize = 1;
+        
+        // int rowmid = n%2+1;
+        
+
+        
+        for(int i=1;i<=n;i++){
+            int m = n;
+            //space
+            for(int j=1;j<= space;j++){
+                System.out.print("  ");    
+            }
+
+            int colmid = patternsize/2 +1 ;
+
+            //patternsize
+            for(int z=1;z<=patternsize;z++){
+                if(z<colmid){
+                    System.out.print(m-- + " ");
+                }
+                else{
+                    System.out.print(m++ + " ");
+                }
+
+            }
+            
+            space --;
+            patternsize +=2 ; 
+            System.out.println();
+        }
+    }
+
+}
