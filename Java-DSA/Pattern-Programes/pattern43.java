75f0d35d5b22887372576cc49639913d532d0d13
Ayush Raturi <ayushraturi6679@gmail.com>
2026-03-31 09:37:57 +0530

    Pattern Added 43 & 44

---
+++ Java-DSA/Pattern-Programes/pattern43.java
@@ -0,0 +1,65 @@
+/*  
+Q82 & 83 same concept 
+
+                                0
+                        1       0       2
+                3       4       0       5       6
+        7       8       9       0       10      11      12
+13      14      15      16      0       17      18      19      20
+
+*/
+
+
+import java.util.Scanner;
+
+public class pattern43{
+
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Rows:");
+        int n = sc.nextInt();
+        sc.close();
+        pattern(n);
+    }
+
+    public static void pattern (int n){
+        // checking the odd or not
+    if (n%2 == 0) {
+        System.out.println("Please Enter odd Number to Execute the Pattern");
+        return;
+    }
+
+        int space = n-1;
+        int patternsize = 1;
+        
+        // int rowmid = n%2+1;
+        
+
+        
+        int m = 1;
+        for(int i=1;i<=n;i++){
+            //space
+            for(int j=1;j<=space;j++){
+                System.out.print("\t");    
+            }
+            
+            int colmid = patternsize/2 + 1 ;
+            
+            //patternsize
+            for(int z=1;z<=patternsize;z++){
+                if(z == colmid){
+                    System.out.print("0" + "\t");
+                }
+                else{
+                    System.out.print(m++ + "\t");
+                }
+
+            }
+            
+            space --;
+            patternsize +=2 ; 
+            System.out.println();
+        }
+    }
+
+}
