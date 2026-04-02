382532f2062dfd0d3b0281ed2708305a2b1435aa
Ayush Raturi <ayushraturi6679@gmail.com>
2026-04-02 18:16:46 +0530

    New Patterns Added

---
+++ Java-DSA/Pattern-Programes/pattern47.java
@@ -0,0 +1,62 @@
+/*  
+Q90
+                                A
+                        A       B       A
+                A       B       C       B       A
+        A       B       C       D       C       B       A
+A       B       C       D       E       D       C       B       A   
+*/
+
+
+import java.util.Scanner;
+
+public class pattern47  {
+
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Rows:");
+        int n = sc.nextInt();
+        sc.close();
+        pattern(n);
+    }
+
+    public static void pattern (int n){
+        // checking the odd or not
+    if (n%2 == 0) {
+        System.out.println("Please Enter odd Number to Execute the Pattern");
+        return;
+    }
+
+        int space = n-1;
+        int patternsize = 1;
+        
+        // int rowmid = n%2+1;
+        
+
+        
+        
+        for(int i=1;i<=n;i++){
+            int m = 1;
+            //space
+            for(int j=1;j<=space;j++){
+                System.out.print("\t");    
+            }
+            
+            int colmid = patternsize/2 + 1 ;
+            
+            //patternsize
+            for(int z=1;z<=patternsize;z++){
+                if(z<colmid)
+            System.out.print((char)(64+m++) + "\t");
+            else
+            System.out.print((char)(64+m--) + "\t");
+            }
+
+            
+            space --;
+            patternsize +=2 ; 
+            System.out.println();
+        }
+    }
+
+}
