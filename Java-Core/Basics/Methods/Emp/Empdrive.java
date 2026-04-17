f601d684612e90c6cc4d3c4f307f34c68fbde226
Ayush Raturi <ayushraturi6679@gmail.com>
2026-04-17 17:29:21 +0530

    Emp changes

---
+++ Java-Core/Basics/Methods/Emp/Empdrive.java
@@ -0,0 +1,34 @@
+public class Empdrive{
+    public static void main(String[] args) {
+        Emp e1=new Emp();
+        Emp e2=new Emp();
+        Emp e3=new Emp();
+        
+        e1.id = 001; e1.name = "John"; e1.department = "IT"; e1.role = "Developer";
+        e2.id = 001; e2.name = "David"; e2.department = "HR"; e2.role = "HR";
+        e3.id = 001; e3.name = "Grey"; e3.department = "Accounts"; e3.role = "CA";
+        
+        System.out.println("Object Refernce: "+ e1);
+        System.out.println();
+        System.out.println("Emp 1 ID : "+ e1.id);
+        System.out.println("Emp 1 Name : "+ e1.name);
+        System.out.println("Emp 1 Deparment : "+ e1.department);
+        System.out.println("Emp 1 Role : "+ e1.role);
+        //Space
+        System.out.println();
+        System.out.println("Object Refernce: "+ e1);
+        System.out.println();
+        System.out.println("Emp 1 ID : "+ e1.id);
+        System.out.println("Emp 1 Name : "+ e1.name);
+        System.out.println("Emp 1 Deparment : "+ e1.department);
+        System.out.println("Emp 1 Role : "+ e1.role);
+        //Space 
+        System.out.println();
+        System.out.println("Object Refernce: "+ e1);
+        System.out.println();
+        System.out.println("Emp 1 ID : "+ e1.id);
+        System.out.println("Emp 1 Name : "+ e1.name);
+        System.out.println("Emp 1 Deparment : "+ e1.department);
+        System.out.println("Emp 1 Role : "+ e1.role);
+    }
+}
